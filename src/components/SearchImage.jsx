209d458cfb285494f08c6315cf0516e7b8a5cb94
Nathan <drejenathan06@gmail.com>
2023-12-05 23:51:01 +0300

    Editted SearchImage Component

--- src/components/SearchImage.jsx
+++ src/components/SearchImage.jsx
@@ -4,7 +4,7 @@ const SearchImage = ({setTerm, term}) => {
   return (
     <div className="flex justify-center m-10 py-3 w-full bg-gradient-to-tr from-slate-400 to-green-200 ">
 
-      <div className="flex justify-start bg-gray-700 rounded-full w-80">
+      <div className="flex  bg-gray-700 rounded-full w-80">
         <input
           className="bg-transparent w-full h-10 p-5 text-white border-spacing-x-16 outline-none"
           type="text"
