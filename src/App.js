aa1f56a89f654c80ccaf06bfaba9992a8cce2fda
Nathan <drejenathan06@gmail.com>
2023-12-05 20:56:55 +0300

    small edit on the CSS

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 function App() {
   return (
-    <div class="max-w-sm rounded overflow-hidden shadow-2xl bg-gradient-to-tr from-slate-500 to-red-400">
-    <img src="https://source.unsplash.com/random" alt="Random pic" className="w-full " />
-    <div className='px-6 py-4'>
-      
-    </div>
+    <div class="max-w-sm rounded overflow-hidden shadow-2xl bg-gradient-to-tr ">
+      <img
+        src="https://source.unsplash.com/random"
+        alt="Random pic"
+        className="w-full "
+      />
+      <div className="px-6 py-4">
+        <div className="font-bold text-blue-500 text-xl mb-2">
+          Picture by Nate
+        </div>
+        <ul className="py-1">
+          <li>
+            <strong>Views:</strong>6040
+          </li>
+          <li>
+            <strong>Downloades:</strong>1200
+          </li>
+          <li>
+            <strong>Likes:</strong>3245
+          </li>
+        </ul>
+      </div>
+      <div className='px-6 py-4'>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #Tag 1
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #Tag 2
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #Tag 3
+        </span>
+      </div>
     </div>
   );
 }
 
-export default App; 
-  
+export default App;
